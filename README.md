542e1c5f10900ed302715155fad547cd08ede332
Lehi Toskin <lehi@tosk.in>
2015-04-12 13:43:34 -0700

    Add README and menu bar

---
+++ README.md
@@ -0,0 +1,11 @@
+DBXplorer
+=========
+
+DBXplorer is a GUI interface that will allow the user to open a given sqlite
+database and view and modify its contents in a sane manner.
+
+## Prerequisites:
+
+- Racket (version 6.0.1)
+
+- Sqlite (version 3)
--- dbxplorer.rkt
+++ dbxplorer.rkt
@@ -1,10 +1,26 @@
 #lang racket/gui
+(require db/base
+         db/sqlite3)
 ; dbxplorer
 ; a gui to explore and modify the contents of a sqlite database
 
 (define frame (new frame%
                    [label "dbxplorer"]
                    [height 600]
-                   [width 800]))
+                   [width 800]
+                   [stretchable-width #t]))
+
+(define menu-bar (new menu-bar%
+                      [parent frame]))
+
+(define menu-file (new menu%
+                       [parent menu-bar]
+                       [label "&File"]))
+
+(define menu-file-open (new menu-item%
+                            [parent menu-file]
+                            [label "&Open"]
+                            [callback (λ (button event)
+                                        (void))]))
 
 (send frame show #t)
